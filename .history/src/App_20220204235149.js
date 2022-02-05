5935759c003d7874daca87f3e1e8eee7a60ef2f5
Jadesola <Jpo12@student.le.ac.uk>
2022-02-05 19:11:22 +0000

    my first commit

---
+++ .history/src/App_20220204235149.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="todo-list">
+          <To/>
+    </div>
+  );
+}
+
+export default App;
---
+++ .history/src/App_20220204235154.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="todo-list">
+          <TodoList/>
+    </div>
+  );
+}
+
+export default App;
